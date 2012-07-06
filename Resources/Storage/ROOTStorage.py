f05c555e8e9c1e3cec044b28b468d6e16cb95f68
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-07-06 17:34:34 +0200

    save

--- Resources/Storage/ROOTStorage.py
+++ Resources/Storage/ROOTStorage.py
@@ -23,6 +23,9 @@ __RCSID__ = "$Id $"
 # @date 2012/06/27 12:42:27
 # @brief Definition of ROOTStorage class.
 
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
 ## imports 
 import os
 import os.path
@@ -37,6 +40,45 @@ from DIRAC.Core.Utilities.Pfn import pfnparse, pfnunparse
 from DIRAC.Core.Utilities.List import breakListIntoChunks
 from DIRAC.Core.Utilities.File import getSize
 from stat import *
+
+def urlsAsList( fcn ):
+  """ checks urls args type and return it as a list of str
+
+  :param self: fcn to call
+  """
+  def wrapper( self, *args, **kwargs ):
+    """ inside worker
+    
+    :warn: urls must be a 1st one arg
+    
+    :param self: instance reference
+    :param args: non-keyword args
+    :param kwargs: keyword args
+    """
+    urls = kwargs["urls"] if "urls" in kwargs else None 
+    if not urls:
+      urls = args[0] if len(args) == 1 else None
+    
+    urlsList = None
+    if type( urls ) in ( str, unicode ):
+      urlsList = [ urls ]
+      #return fcn( self, urls = [ urls ] )
+    elif type( urls ) in ( dict, list ):
+      urlsList = [ key for key in urls if type(key) in ( unicode, str ) ]
+      if len( urlsList ) != len( urls ):
+        return S_ERROR("%s wrong args type for urls!" % self.fcn.__name__ )  
+    else:
+      return S_ERROR("%s wrong args type for urls!" % self.fcn.__name__ )  
+    ## update args & kwargs
+    if "urls" not in kwargs:
+      args = list( args )
+      del args[0] 
+      args = tuple( args )
+    kwargs["urls"] = urlsList 
+    return fcn( self, *args, **kwargs  )        
+    
+  return wrapper
+
 
 ########################################################################
 class ROOTStorage( StorageBase ):
@@ -78,89 +120,32 @@ class ROOTStorage( StorageBase ):
     ## xrd timeout
     self.xrdTimeout = gConfig.getValue( "/Resources/StorageElements/XRDTimeout", 300 )
     self.xrdRetry = gConfig.getValue( "/Resources/StorageElements/XRDRetry", 3 )
+      
 
-  
-  def isPfnForProtocol( self, pfn ):
-    """ check if supplied pfn is valid for XROOT protocol
+  ###
+  ### SURL/PFN/protocol/cwd/params manipulation
+  ###
 
-    :param self: self reference
-    :param str pfn: PFN
-    """
-    res = pfnparse( pfn )
-    return S_OK( res if not res["OK"] else res["Value"]["Protocol"] == self.protocol ) 
-
-  def changeDirectory( self, directory ):
-    """ cd dir
+  def resetWorkingDirectory( self ):
+    """ cd self.path
 
     :param self: self reference
-    :param str directory:
     """
-    self.cwd = os.path.normpath( os.path.join( self.cwd, directory.lstrip("/") ) )
+    self.cwd = self.path
 
-  def createDirectory( self, path ):
-    """ mkdir path
-
-    :param self: self reference
-    :param str path: path to create
-    """
+  def getCurrentDirectory( self ):
+    """ get cwd
     
-    pass
-
-  def getCurrentURL( self ):
-    """ get full SE URL including fileName
-
     :param self: self reference
     """
+    return S_OK( self.cwd )
+
+  def getName( self ):
+    """ get SE name
     
-
-    pass
-
-  def getDirectory( self, path ):
-    """ cp -r "SE path" . 
-
     :param self: self reference
     """
-    pass
-
-
-  def getDirectoryMetadata( self, path ):
-    """ ls -l, but not really 
-
-    :param self: self reference
-
-    """
-    pass
-
-  def getDirectorySize( self , path ):
-    """ du -s dir
-
-    :param self: self reference
-    """
-    pass
-
-  def getFile( self, path ):
-    """ cp SE localFS
-
-    :param self: self reference
-    """
-
-    pass
-
-  def getFileMetadata( self, path  ):
-    """ ls -l file
-
-    :param self: self reference
-    """
-
-    pass
-  
-  def getFileSize( self, path ):
-    """ du file
-
-    :param self: self reference
-    """
-
-    pass
+    return S_OK( self.name )
 
   def getParameteres( self ):
     """ get the original parameters dict 
@@ -176,16 +161,172 @@ class ROOTStorage( StorageBase ):
                    "SpaceToken" : self.spaceToken,
                    "WSUrl" : self.wspath } )
 
+  def isPfnForProtocol( self, pfn ):
+    """ check if supplied pfn is valid for XROOT protocol
+
+    :param self: self reference
+    :param str pfn: PFN
+    """
+    res = pfnparse( pfn )
+    return res if not res["OK"] else S_OK( res["Value"]["Protocol"] == self.protocol ) 
+
+  
+  def getCurrentURL( self, fileName ):
+    """ get full SE URL including fileName
+
+    :param self: self reference
+    :param str fileName: fine name
+    """
+    return S_OK( "%s/%s" % ( self.cwd, fileName.lstrip("/") if fileName else "" ) )  
+
+  def getProtocolPfn( self, pfnDict, withPort ):
+    """ contruct SURL to be used from :pfnDict:
+    
+    :param self: self reference
+    :param dict pfnDict: pfn dict
+    :param bool withPort: include port information
+    """
+    pfnDict["Protocol"] = self.protocol
+    pfnDict["Host"] = self.host    
+    pfnDict["Port"] = self.port if withPort else ""
+    pfnDict["WSUrl"] = self.wspath if withPort else ""
+    return  pfnunparse( pfnDict )
+
+  def getTransportURL( self ):
+    """
+
+    :param self: self reference
+    """
+    pass
+
+  ###
+  ### directory manipulation
+  ###
+
+  def changeDirectory( self, directory ):
+    """ cd dir 
+
+    :param self: self reference
+    :param str directory:
+    """
+    self.cwd = os.path.normpath( os.path.join( self.cwd, directory.lstrip("/") ) )
+
+  @urlsAsList  
+  def createDirectory( self, urls ):
+    """ mkdir path
+
+    TODO: need to use putFile with temp dir
+
+    :param self: self reference
+    :param str urls: path to create
+    """
+    successful = []
+    failed = {}
+    for url in urls:
+      url2create = self.path
+      for path in [ path.strip() for path in url.rstrip( self.path ).split("/") if path.strip() ]:
+        url2create += "/%s" % path 
+        self.log.always( "mkdir %s " % url2create )
+        #res = { "OK" : True, "Value" : (0, "", "" ) }
+        res = self.__xrd_wrapper( "mkdir", url2create )
+        if not res["OK"]:
+          self.log.error( "createDirectory: Failed to create directory on storage.", url2create )
+          failed[url] = res['Message']
+          continue
+        exitCode, stdOut, stdErr = res["Value"]
+        if exitCode:
+          self.log.error( "createDirectory: Failed to create directory on storage.", url )
+          failed[url] = stdErr
+          continue
+      self.log.debug( "createDirectory: Successfully created directory on storage: %s" % url )
+      successful.append(url)
+    return S_OK( { "Successful" : dict.fromkeys( successful, True ), 
+                   "Failed" : failed } ) 
+
+  @urlsAsList
+  def getDirectory( self, urls, localPath=None ):
+    """ cp -r "SE path" . 
+
+    :param self: self reference
+    :param mixed urls: source
+    :param str localPath: cp destination (if omitted, cwd will be used)
+    """
+    pass
+
+  @urlsAsList
+  def getDirectoryMetadata( self, urls ):
+    """ ls -l, but not really 
+
+    :param self: self reference
+    """
+    pass
+
+  @urlsAsList
+  def getDirectorySize( self, urls ):
+    """ du -s dir
+
+    :param self: self reference
+    :param mixed urls: paths to check
+    """
+    pass
+
+  @urlsAsList
+  def getFile( self, urls ):
+    """ cp SE localFS
+
+    :param self: self reference
+    """
+
+    pass
+
+  @urlsAsList
+  def getFileMetadata( self, urls ):
+    """ ls -l file
+
+    :param self: self reference
+    """
+    pass
+
+  @urlsAsList
+  def getFileSize( self, urls ):
+    """ du file
+
+    :param self: self reference
+    :param mixed urls: paths to check
+    """
+    isFile = self.isFile( urls )
+    if not isFile["OK"]:
+      return isFile
+    isFile = isFile["Value"]
+
+    failed = isFile["Failed"]
+    successful = {}
+    
+    for url in isFile["Successful"]:
+      res = self.__xrd_wrapper( "stat", url )
+      if not res["OK"]:
+        failed[url] = res["Message"]
+        continue
+      exitCode, stdOut, stdErr = res["Value"]
+      if exitCode:
+        failed["url"] = stdErr
+        continue
+      try:
+        size = int(stdOut.strip().split()[3].strip())
+      except ( TypeError, ValueError ), error:
+        failed[url] = str(error)
+        continue
+      successful[url] = size
+
+    return S_OK( { "Successful" : successful, "Failed" : failed } )
+      
+  @urlsAsList
   def exists( self, urls ):
     """ test -d or test -f
 
     :param self: self reference
     :param mixed urls: urls to check 
-    """
-    urls = self.__checkArgumentFormat( urls )
-    if not urls["OK"]:
-      return urls
-    
+    """    
     successful = []
     failed = {}
     isDirectory = self.isDirectory( urls )
@@ -201,33 +342,16 @@ class ROOTStorage( StorageBase ):
     return S_OK( { "Successful" : dict.fromkeys( successful.keys(), True), 
                    "Failed" : dict.fromkeys( [ url for url in urls if url not in successful.keys() ], False ) } )
     
-  def getProtocolPfn( self ):
-    """
-
-    :param self: self reference
-    """
-
-    pass
-
-  def getTransportURL( self ):
-    """
-
-    :param self: self reference
-    """
-    pass
-
+  @urlsAsList
   def isDirectory( self, urls ):
     """ test -d dir
 
     :param self: self reference
     :param mixed urls: urls to check
     """
-    urls = self.__checkArgumentFormat( urls )
-    if not urls["OK"]:
-      return urls
     successful = []
     failed = []
-    for url in urls["Value"]:
+    for url in urls:
       res = self.__xrd_wrapper( "existdir", url )
       if not res["OK"]:
         failed.append( ( url, res["Message"] ) )
@@ -240,18 +364,16 @@ class ROOTStorage( StorageBase ):
       failed.append( ( url, False ) )
     return S_OK( { "Successful" : dict.fromkeys( successful, True ), "Failed" : dict(failed) } )
 
+  @urlsAsList
   def isFile( self, urls ):
     """ test -f file
 
     :param self: self reference
     :param mixed urls: paths to check
     """
-    urls = self.__checkArgumentFormat( urls )
-    if not urls["OK"]:
-      return urls
     successful = []
     failed = []
-    for url in urls["Value"]:
+    for url in urls:
       res = self.__xrd_wrapper( "existfile", url )
       self.log.always( res )
       if not res["OK"]:
@@ -266,16 +388,14 @@ class ROOTStorage( StorageBase ):
 
     return S_OK( { "Successful": dict.fromkeys( successful, True ), "Failed" : dict(failed) } )  
 
+  @urlsAsList
   def listDirectory( self, urls ):
     """ ls dir
     
     :param self: self reference
     :param mixed urls: folders to check
     """
-    urls = self.__checkArgumentFormat( urls )
-    if not urls["OK"]:
-      return urls
-    isDirectory = self.isDirectory( urls["Value"] )
+    isDirectory = self.isDirectory( urls )
     if not isDirectory["OK"]:
       return isDirectory
     urls = isDirectory["Value"]["Successful"].keys()
@@ -298,73 +418,89 @@ class ROOTStorage( StorageBase ):
         if permission.startswith("d"):
           subDirs[path] = True 
         else:
-          files[path] = { "Size" : int(size) }
+          files[path] = { "Size" : int(size), 
+                          "Permission" : permission, 
+                          "CTime" : "%s %s" % ( cDate, cTime ) }
       successful[url] = { "SubDirs" : subDirs, "Files" : files }
     return S_OK( { "Successful" : successful, "Failed" : failed } )
          
-  def pinFile( self ):
+  @urlsAsList
+  def pinFile( self, urls ):
     """ pin file 
 
     :param self: self reference
     """
     pass
-
-  def prestageFile( self ):
+  @urlsAsList
+  def prestageFile( self, urls ):
     """ prestage file
 
     :param self: self reference
     """
     pass
 
-  def prestageFileStatus( self ):
+  @urlsAsList
+  def prestageFileStatus( self, urls ):
     pass
 
-  def putFile( self ):
+  @urlsAsList
+  def putFile( self, urls ):
     """ cp localFS SE
  
     :param self: self reference
     """
     pass
 
-  def releaseFile( self ):
+  @urlsAsList
+  def releaseFile( self, urls ):
     """ un-pin file
 
     :param self: self reference
     """
     pass
 
-  def removeDirectory( self ):
+  @urlsAsList
+  def removeDirectory( self, urls ):
     """ rm or even rm -rf 
 
     :param self: self referece
     """
-
+    successful = {}
+    failed = {}
     pass
 
-  def removeFile( self ):
+  @urlsAsList
+  def removeFile( self, urls ):
     """ rm file
 
     :param self: self reference
     """
     pass
-
   
-  def __checkArgumentFormat( self, path ):
-    """ check and convert :path: to list of paths 
+  ###
+  ### xrd, xrdcp, xprep wrappers + __executeOperation
+  ### 
 
+  def __executeOperation( self, url, method ):
+    """ execute the :method: with the supplied :urls:
+    
     :param self: self reference
-    :param mixed path: arg to check  
+    :param mixed url: urls to use
+    :param str method: member fcn name 
     """
-    if type( path ) in StringTypes:
-      urls = [ path ]
-    elif type( path ) == ListType:
-      urls = path
-    elif type( path ) == DictType:
-      urls = path.keys()
-    else:
-      return S_ERROR( "__checkArgumentFormat: Supplied path is not of the correct format." )
-    return S_OK( urls )
-  
+    fcn = None
+    if hasattr(self, method) and callable( getattr(self, method) ):
+      fcn = getattr( self, method )
+    if not fcn:
+      return S_ERROR("Unable to invoke %s, it isn't a member funtion of ROOTStorage" % method )
+    res = fcn( url )
+    if not res["OK"]:
+      return res
+    elif url not in res["Value"]["Successful"]:
+        return S_ERROR( res["Value"]["Failed"][url] )
+    return S_OK( res["Value"]["Successful"][url] )
+
+
   def __xrd_wrapper( self, operation, url, timeout=None, callback=None ):
     """ xrd wrapper calling :operation: on :url:
 
@@ -424,15 +560,14 @@ class ROOTStorage( StorageBase ):
 
 # TODO remove when ready
 if __name__ == "__main__":
-  from DIRAC.Core.Base.Script import parseCommandLine
-  parseCommandLine()
+ 
   from DIRAC.Resources.Storage.StorageFactory import StorageFactory
   sf = StorageFactory()
   rs = sf.getStorages( "CERN-USER" )["Value"]["StorageObjects"][0]
-  gLogger.always( rs.getParameteres() )
-  res = rs.isDirectory( [ "/user/c/cibak", "user/c/cibak/a", "user/c/cibak/b"] )
-  res = rs.isFile( [ "user/c/cibak/cert-test2"] )
-
+  #gLogger.always( rs.getParameteres() )
+  #res = rs.isDirectory( [ "/user/c/cibak", "user/c/cibak/a", "user/c/cibak/b"] )
+  #res = rs.isFile( [ "user/c/cibak/cert-test2"] )
   res = rs.listDirectory( "user/c/cibak/" )
-
+  #res = rs.getFileSize( [ "user/c/cibak/cert-test2", "user/c/cibak/a" ] )
+  #res = rs.createDirectory( "/user/c/cibak/a/b/c/d/e/f/g" )
   gLogger.always( res )
