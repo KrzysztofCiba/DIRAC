dd71e8e5b82e2463999324c069a1270cfcc2a5d6
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-08-03 17:04:44 +0200

    save

---
+++ RequestManagementSystem/Client/SubReqFile.py
@@ -0,0 +1,42 @@
+########################################################################
+# $HeadURL $
+# File: SubReqFile.py
+# Author: Krzysztof.Ciba@NOSPAMgmail.com
+# Date: 2012/08/03 15:02:53
+########################################################################
+
+""" :mod: SubReqFile 
+    ================
+ 
+    .. module: SubReqFile
+    :synopsis: sub-request file
+    .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
+
+    sub-request file
+"""
+
+__RCSID__ = "$Id $"
+
+##
+# @file SubReqFile.py
+# @author Krzysztof.Ciba@NOSPAMgmail.com
+# @date 2012/08/03 15:03:03
+# @brief Definition of SubReqFile class.
+
+## imports 
+
+
+########################################################################
+class SubReqFile(object):
+  """
+  .. class:: SubReqFile
+  
+  """
+
+  def __init__( self ):
+    """c'tor
+
+    :param self: self reference
+    """
+    pass
+
