d7bb4d20b492bfefd13e3f0bf6da1fe15cf2d085
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-08-09 16:59:04 +0200

    save

--- Core/Utilities/Traced.py
+++ Core/Utilities/Traced.py
@@ -46,7 +46,7 @@ class Traced( type ):
       """
       if not self.__update__ or reset:
         self.__update__ = list()
-      if element and element not in self.__update__:
+      if element != None and element not in self.__update__:
         self.__update__.append( element )
       return self.__update__
 
@@ -56,18 +56,34 @@ class Traced( type ):
         if not hasattr( self, name ) or getattr( self, name ) != value:
           self.updated( name )
       bases[0].__setattr__( self, name, value )
-      
+    
+    def trace_update( self, seq ):      
+      """ for dict only """
+      for key, value in seq.items():
+        if key not in self or bases[0].__getitem__( self, key ) != value:
+          self.updated( key )
+      bases[0].update( seq )
+   
+    def trace_append( self, item ):
+      """ append for list """
+      self.updated( len(self) )
+      self += [ item ] 
+
     def trace_setitem( self, ind, item ):
       """ __setitem__ tracing value update """
-      try:
-        if bases[0].__getitem__( self, ind ) != item:
-          self.updated( ind )
-      except KeyError:
+      if bases[0] == dict and ( ind not in self or bases[0].__getitem__( self, ind ) != item ):
+        self.updated( ind )
+      elif bases[0] == list and bases[0].__getitem__( self, ind ) != item:
         self.updated( ind )
       bases[0].__setitem__( self, ind, item )
    
     classdict["__setattr__"] = trace_setattr
     classdict["__setitem__"] = trace_setitem
+    if bases[0] == dict:
+      classdict["update"] = trace_update
+    if bases[0] == list:
+      classdict["append"] = trace_append
+
     classdict["updated"] = updated 
     classdict["__update__"] = None
 
@@ -76,7 +92,7 @@ class Traced( type ):
 class TracedDict(dict):
   """ traced dict """
   __metaclass__ = Traced
-
+    
 class TracedList(list):
   """ traced list """
   __metaclass__ = Traced
