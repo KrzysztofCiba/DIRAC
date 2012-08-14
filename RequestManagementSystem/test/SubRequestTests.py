c54e4b0495043ffe90927117d89a9c0ca2d765fa
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-08-14 21:26:59 +0200

    save

--- RequestManagementSystem/test/SubRequestTests.py
+++ RequestManagementSystem/test/SubRequestTests.py
@@ -26,7 +26,7 @@ __RCSID__ = "$Id $"
 ## imports 
 import unittest
 ## from DIRAC
-from DIRAC.RequestManagementSystem.Client.SubReqFile import SubRequFile
+from DIRAC.RequestManagementSystem.Client.SubReqFile import SubReqFile
 ## SUT
 from DIRAC.RequestManagementSystem.Client.SubRequest import SubRequest
 
@@ -38,11 +38,38 @@ class SubRequestTests(unittest.TestCase):
   """
 
   def setUp( self ):
-    """c'tor
+    """ test set up """
+    self.fromDict = { "SubRequestID" : 1,
+                      "RequestType" : "transfer",
+                      "Operation" : "replicateAndRegister",
+                      "TargetSE" : "CERN-USER,PIC-USER" }
+    self.subFile = SubReqFile( { "LFN" : "/lhcb/user/c/cibak/testFile",
+                                 "Addler" : "1234567",
+                                 "Size" : 1024 } )
+    
+  def tearDown( self ):
+    """ test case tear down """
+    del self.fromDict
+    del self.subFile
 
-    :param self: self reference
-    """
-    pass
+
+  def testCtor( self ):
+    """ test ctors """
+    ## empty ctor
+    self.assertEqual( isinstance( SubRequest(), SubRequest), True )
+    ## using fromDict
+    subReq = SubRequest( self.fromDict )
+    self.assertEqual( isinstance( subReq, SubRequest), True )
+    for key, value in self.fromDict.items():
+      self.assertEqual( getattr(subReq, key), value )
+    ## from XML
+    subReq = SubRequest.fromXML( subReq.toXML() )
+    self.assertEqual( isinstance( subReq, SubRequest), True )
+    for key, value in self.fromDict.items():
+      self.assertEqual( getattr(subReq, key), value )
+
+
+
 
 
 ## test execution
