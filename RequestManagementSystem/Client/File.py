8abc2d356e6c45628b20a3645df313801d115f92
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2012-12-02 20:11:57 +0100

    save

--- RequestManagementSystem/Client/File.py
+++ RequestManagementSystem/Client/File.py
@@ -75,158 +75,129 @@ class File( object ):
     return str(self) == str(other)
 
   ## props  
-  def __fileID():
-    """ file ID """
-    doc = "FileID"
-    def fset( self, value ):
-      """ FileID setter """
-      value = long(value) if value else None
-      self.__data__["FileID"] = value
-    def fget( self ):
-      """ FileID getter """
-      return self.__data__["FileID"]
-    return locals()
-  FileID = property( **__fileID() ) 
-
-  def __operationID():
-    """ operation ID, this one is ro """
-    doc = "operation ID"
-    def fset( self, value ):
-      """ OperationID setter """
-      value = long(value) if value else None
-      if self._parent and self._parent.OperationID != value:
-        raise ValueError("parent OperationID mismatch (%s != %s)" % ( self._parent.OperationID, value ) )
-      self.__data__["OperationID"] = value
-    def fget( self ):
-      """ OperationID getter """
-      if self._parent:
-        self.__data__["OperationID"] = self._parent.OperationID
-      return self.__data__["OperationID"]
-    return locals()
-  OperationID = property( **__operationID() )
-
-  def __size():
-    """ file size prop """
-    doc = "file size in bytes"
-    def fset( self, value ):
-      """ file size setter """
-      value = long(value)
-      if value < 0:
-        raise ValueError("Size should be a positive integer!")
-      self.__data__["Size"] = value
-    def fget( self ):
-      """ file size getter """
-      return self.__data__["Size"]
-    return locals()
-  Size = property( **__size() )
-
-  def __lfn():
-    """ LFN prop """
-    doc = "LFN"
-    def fset( self, value ):
-      """ lfn setter """
-      if type(value) != str:
-        raise TypeError("LFN has to be a string!")
-      if not os.path.isabs( value ):
-        raise ValueError("LFN should be an absolute path!")
-      self.__data__["LFN"] = value
-    def fget( self ):
-      """ lfn getter """
-      return self.__data__["LFN"]
-    return locals()
-  LFN = property( **__lfn() )
-
-  def __pfn():
-    """ PFN prop """
-    doc = "PFN"
-    def fset( self, value ):
-      """ PFN setter """
-      if value:
-        if type(value) != str:
-          raise TypeError("PFN has to be a string!")
-        if not urlparse.urlparse( value ).scheme:
-          raise ValueError("Wrongly formatted URI!")
-      self.__data__["PFN"] = value
-    def fget( self ):
-      """ PFN getter """
-      return self.__data__["PFN"]
-    return locals()
-  PFN = property( **__pfn() )
-
-  def __guid():
-    """ GUID prop """
-    doc = "GUID"
-    def fset( self, value ):
-      """ GUID setter """
-      if value:
-        if type(value) not in ( str, unicode ):
-          raise TypeError("GUID should be a string!")
-        if not checkGuid( value ):
-          raise ValueError("'%s' is not a valid GUID!" % str(value) )
-      self.__data__["GUID"] = value
-    def fget( self ):
-      """ GUID getter """
-      return self.__data__["GUID"]
-    return locals()
-  GUID = property( **__guid() )
-
-  def __checksumType():
-    """ checksum type prop """
-    doc = "ChecksumType"
-    def fset( self, value = "NONE" ):
-      """ checksum type setter """
-      value = str(value).upper()
-      if value not in ( "ADLER", "MD5", "SHA1", "NONE" ):
-        raise ValueError("unknown checksum type: %s" % value )
-      self.__data__["ChecksumType"] = value
-    def fget( self ):
-      """ ChecksumType getter """
-      return self.__data__["ChecksumType"]
-    return locals()
-  ChecksumType = property( **__checksumType() )
-
-  def __checksum():
-    """ checksum prop """
-    doc = "checksum"
-    def fset( self, value ):
-      """ checksum setter """
-      self.__data__["Checksum"] = str(value)
-    def fget( self ):
-      """ checksum getter """
-      return self.__data__["Checksum"]
-    return locals()
-  Checksum = property( **__checksum() ) 
+  @property.getter
+  def fileID( self ):
+    """ FileID getter """
+    return self.__data__["FileID"]
   
-  def __error():
-    """ error prop """
-    doc = "error"
-    def fset( self, value ):
-      """ error setter """
-      if type(value) != str:
-        raise TypeError("Error has to be a string!")
-      self.__data__["Error"] = value[255:]
-    def fget( self ):
-      """ error getter """
-      return self.__data__["Error"]
-    return locals()
-  Error = property( **__error() )
-    
-  def __status():
-    """ status prop """
-    doc = "file status"
-    def fset( self, value ):
-      """ status setter """
-      if value not in ( "Waiting", "Failed", "Done", "Scheduled" ):
-        raise ValueError( "Unknown Status: %s!" % str(value) )
-      self.__data__["Status"] = value
-    def fget( self ):
-      """ status getter """
-      if not self.__data__["Status"]:
-        self.__data__["Status"] = "Waiting"
-      return self.__data__["Status"] 
-    return locals()
-  Status = property( **__status() )
+  @property.setter
+  def fileID( self, value ):
+    """ FileID setter """
+    value = long(value) if value else None
+    self.__data__["FileID"] = value
 
+  @property.getter
+  def operationID( self ):
+    """ operation ID, this one is ro """
+    if not self._parent:
+      raise AttributeError( "parent operation not set!")
+    return self._parent.operationID
+
+  @property.getter
+  def size( self ):
+    """ file size getter """
+    return self.__data__["Size"]
+
+  @property.setter
+  def size( self, value ):
+    """ file size setter """
+    value = long(value)
+    if value < 0:
+      raise ValueError("Size should be a positive integer!")
+    self.__data__["Size"] = value
+   
+  @property.getter
+  def lfn( self ):
+    """ LFN prop """
+    return self.__data__["LFN"]
+    
+  @property.setter
+  def lfn( self, value ):
+    """ lfn setter """
+    if type(value) != str:
+      raise TypeError("LFN has to be a string!")
+    if not os.path.isabs( value ):
+      raise ValueError("LFN should be an absolute path!")
+    self.__data__["LFN"] = value
+
+  @property.getter
+  def pfn( self ):
+    """ PFN prop """
+    return self.__data__["PFN"]
+  
+  @property.setter
+  def pfn( self, value ):
+    """ PFN setter """
+    if type(value) != str:
+      raise TypeError("PFN has to be a string!")
+    if not urlparse.urlparse( value ).scheme:
+      raise ValueError("Wrongly formatted URI!")
+    self.__data__["PFN"] = value
+
+  @property.getter
+  def guid( self ):
+    """ GUID prop """
+    return self.__data__["GUID"]
+
+  @property.setter
+  def guid( self, value ):
+    """ GUID setter """
+    if value:
+      if type(value) not in ( str, unicode ):
+        raise TypeError("GUID should be a string!")
+      if not checkGuid( value ):
+        raise ValueError("'%s' is not a valid GUID!" % str(value) )
+    self.__data__["GUID"] = value
+    
+  @property.getter
+  def checksumType( self ):
+    """ checksum type prop """
+    return self.__data__["ChecksumType"]
+
+  @property.setter
+  def checksumType( self, value = "NONE" ):
+    """ checksum type setter """
+    value = str(value).upper()
+    if value not in ( "ADLER", "MD5", "SHA1", "NONE" ):
+      raise ValueError("unknown checksum type: %s" % value )
+    self.__data__["ChecksumType"] = value
+
+  @property.getter
+  def checksum( self ):
+    """ checksum prop """
+    return self.__data__["Checksum"]
+  
+  @property.setter
+  def checksum( self, value ):
+    """ checksum setter """
+    self.__data__["Checksum"] = str(value)
+  
+  @property.getter
+  def error( self ):
+    """ error prop """
+    return self.__data__["Error"]
+  
+  @property.setter
+  def error( self, value ):
+    """ error setter """
+    if type(value) != str:
+      raise TypeError("Error has to be a string!")
+    self.__data__["Error"] = value[255:]
+
+  @property.getter
+  def status( self ):
+    """ status prop """
+    if not self.__data__["Status"]:
+      self.__data__["Status"] = "Waiting"
+    return self.__data__["Status"] 
+
+  @property.setter
+  def status( self, value ):
+    """ status setter """
+    if value not in ( "Waiting", "Failed", "Done", "Scheduled" ):
+      raise ValueError( "Unknown Status: %s!" % str(value) )
+    self.__data__["Status"] = value
+  
   ## (de)serialisation   
   def toXML( self ):
     """ serialise File to XML """
