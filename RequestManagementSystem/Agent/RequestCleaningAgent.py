2c9fe7117c4fb40c96e99f86ef6e51d9db6ea138
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-01-23 16:31:57 +0100

    save

--- RequestManagementSystem/Agent/RequestCleaningAgent.py
+++ RequestManagementSystem/Agent/RequestCleaningAgent.py
@@ -1,35 +1,38 @@
 ########################################################################
 # $HeadURL:  $
 ########################################################################
+""" :mod: RequestCleaningAgent
+    ==========================
 
-"""  The RequestCleaning agent removes the already executed requests from the database after
-     a grace period.
+    .. module: RequestCleaningAgent
+    :synopsis: The RequestCleaningAgent removes the already executed requests from the database afte a grace period.
 """
+## rscid
+__RCSID__ = "$Id: $" 
 
-from DIRAC  import gLogger, S_OK, S_ERROR
+# imports
+from DIRAC import S_OK, S_ERROR
 from DIRAC.Core.Base.AgentModule import AgentModule
 from DIRAC.RequestManagementSystem.Client.RequestClient import RequestClient
-from DIRAC.Core.Utilities.Time import dateTime,day,second
-
-__RCSID__ = "$Id: $"
-
-AGENT_NAME = 'RequestManagement/ZuziaAgent'
+from DIRAC.Core.Utilities.Time import dateTime, day, second
+## agent name
+AGENT_NAME = 'RequestManagement/RequestCleaningAgent'
 
 class RequestCleaningAgent(AgentModule):
+  """
+  .. class:: RequestCleanignAgent
 
+  """
   def initialize(self):
-
+    """ agent initialisation """
     self.graceRemovalPeriod = self.am_getOption('GraceRemovalPeriod',7)
     self.checkAssigned = self.am_getOption('CheckAssigned',True)
     self.assignedResetDelay = self.am_getOption('AssignedResetDelay',7200)
-    self.ftsCleaning = self.am_getOption('FTSCleaning',True)
     self.requestClient = RequestClient()
-    
     return S_OK()
 
   def execute(self):
-    """ Main execution method
-    """
+    """ Main execution method """
     
     toDate = dateTime() - day*self.graceRemovalPeriod
     result = self.requestClient.selectRequests({'Status':'Done','ToDate':str(toDate)})
@@ -38,28 +41,25 @@ class RequestCleaningAgent(AgentModule):
     requestDict = result['Value']
     for rID,rName in requestDict.items():
 
-      gLogger.verbose("Removing request %s" % rName)
+      self.log.verbose("Removing request %s" % rName)
       result = self.requestClient.deleteRequest(rName)
       if not result['OK']:
-        gLogger.error('Failed to delete request %s' % rName, result['Message'])
+        self.log.error( 'Failed to delete request %s' % rName, result['Message'] )
       else:
-        gLogger.info('Successfully removed request %d/%s' % (rID,rName) )
+        self.log.info( 'Successfully removed request %d/%s' % ( rID, rName ) )
       
     if self.checkAssigned:
       toDate = dateTime() - second*self.assignedResetDelay
-      result = self.requestClient.selectRequests({'Status':'Assigned','ToDate':str(toDate)})
+      result = self.requestClient.selectRequests( {'Status' : 'Assigned', 'ToDate' : str(toDate) } )
       if not result['OK']:
         return result
       requestDict = result['Value']
-      for rID,rName in requestDict.items():
-        gLogger.verbose("Resetting request %s to Waiting" % rName)
-        result = self.requestClient.setRequestStatus(rName,'Waiting')
+      for rID, rName in requestDict.items():
+        self.log.verbose( 'Resetting request %s to Waiting' % rName )
+        result = self.requestClient.setRequestStatus( rName, 'Waiting' )
         if not result['OK']:
-          gLogger.error('Failed to reset request %s to Waiting' % rName, result['Message'])
+          self.log.error( 'Failed to reset request %s to Waiting' % rName, result['Message'] )
         else:
-          gLogger.info('Successfully reset request %d/%s to Waiting' % (rID,rName) )
-
-    if self.ftsCleaning:
-      pass
+          self.log.info( 'Successfully reset request %d/%s to Waiting' % ( rID, rName ) )
     
     return S_OK()  
