613d788ec4c6159105e5e9571d5b5e310f78537b
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-03-24 22:54:20 +0100

    save

---
+++ RequestManagementSystem/private/ForwardDISET.py
@@ -0,0 +1,59 @@
+########################################################################
+# $HeadURL $
+# File: ForwardDISET.py
+# Author: Krzysztof.Ciba@NOSPAMgmail.com
+# Date: 2013/03/22 12:40:06
+########################################################################
+
+""" :mod: ForwardDISET 
+    =======================
+ 
+    .. module: ForwardDISET
+    :synopsis: DISET forwarding operation handler
+    .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
+
+    DISET forwarding operation handler
+"""
+
+__RCSID__ = "$Id $"
+
+##
+# @file ForwardDISET.py
+# @author Krzysztof.Ciba@NOSPAMgmail.com
+# @date 2013/03/22 12:40:22
+# @brief Definition of ForwardDISET class.
+
+## imports 
+from DIRAC import S_OK, S_ERROR, gMonitor
+from DIRAC.RequestManagementSystem.private.BaseOperation import BaseOperation
+from DIRAC.Core.DISET.RPCClient import executeRPCStub
+from DIRAC.Core.Utilities import DEncode
+
+
+########################################################################
+class ForwardDISET(BaseOperation):
+  """
+  .. class:: ForwardDISET
+  
+  """
+  def __init__( self, operation ):
+    """ c'tor """
+    BaseOperation.__init__( self, operation )
+    
+  def __call__( self ):
+    """ execute RPC stub """
+    ## update monitor for attempted
+    self.monitor().addMark( "Attempted", 1 )
+    ## get arguments
+    rpcStubString = self.operation.Arguments
+    rpcStub, length = DEncode.decode( rpcStubString )
+    forward = executeRPCStub( rpcStub )
+    if not forward["OK"]:
+      self.monitor().addMark( "Failed", 1 )
+      self.log.error("unable to execute '%s' operation: %s" % ( self.operation.Type, forward["Message"] ) )
+      return forward
+    self.monitor().addMark( "Successful", 1 )
+    self.operation.Status = "Done"
+    return S_OK()
+    
+    
