704781195dacb675c8af2bed709a9e1f8715b576
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-03-26 11:08:31 +0100

    save

--- DataManagementSystem/private/RemoveFile.py
+++ DataManagementSystem/private/RemoveFile.py
@@ -24,7 +24,7 @@ __RCSID__ = "$Id $"
 # @brief Definition of RemoveFile class.
 
 ## imports 
-from DIRAC import S_OK, S_ERROR, gMonitor
+from DIRAC import S_OK, gMonitor
 from DIRAC.RequestManagementSystem.private.BaseOperation import BaseOperation
 
 ########################################################################
@@ -42,14 +42,128 @@ class RemoveFile(BaseOperation):
     ## call base class ctor
     BaseOperation.__init__(self, operation)
     ## gMonitor stuff goes here
-    gMonitor.registerActivity( "FileRemovalsAttempted", "File removals attempted", 
+    gMonitor.registerActivity( "FileRemovalsAtt", "File removals attempted",
                                "RemoveFile", "Files/min", gMonitor.OP_SUM )
-    gMonitor.registerActivity( "FileRemovalsFaild", "File removals failed", 
+    gMonitor.registerActivity( "FileRemovalsFail", "File removals failed",
                                "RemoveFile", "Files/min", gMonitor.OP_SUM )
-    gMonitor.registerActivity( "FileRemovalsSuccesful", "File removals successful", 
+    gMonitor.registerActivity( "FileRemovalsSucc", "File removals successful",
                                "RemoveFile", "Files/min", gMonitor.OP_SUM )
 
   def __call__( self ):
-    pass
+    """ action for 'removeFile' operation  """
 
+    lfns = [ opFile.LFN for opFile in self.operation if opFile.Status == "Waiting" ]
+    self.log.debug( "about to remove %d files" % len( lfns ) )
+    # # keep removal status for each file
+    removalStatus = dict.fromkeys( lfns, "" )
+    gMonitor.addMark( "FileRemovalsAtt", len( lfns ) )
+
+    # # bulk removal 1st
+    bulkRemoval = self.replicaManager().removeFile( lfns )
+    if not bulkRemoval["OK"]:
+      self.log.error( "unable to remove files: %s" % bulkRemoval["Message"] )
+      self.operation.Error = bulkRemoval["Message"][:255]
+      return bulkRemoval
+
+    bulkRemoval = bulkRemoval["Value"]
+    failedLfns = bulkRemoval["Failed"] if "Failed" in bulkRemoval else []
+    toRemove = []
+    for lfn in removalStatus:
+      if lfn in failedLfns and "no such file or directory" in str( bulkRemoval["Failed"][lfn] ).lower():
+        removalStatus[lfn] = bulkRemoval["Failed"][lfn]
+        removeCatalog = self.replicaManager().removeCatalogFile( lfn, singleFile = True )
+        if not removeCatalog["OK"]:
+          removalStatus[lfn] = removeCatalog["Message"]
+          continue
+      else:
+        toRemove.append( lfn )
+
+    # # loop over LFNs to remove
+    for lfn in toRemove:
+      self.debug( "removeFile: processing file %s" % lfn )
+      try:
+        # # try to remove using proxy already defined in os.environ
+        removal = self.replicaManager().removeFile( lfn )
+        # # file is not existing?
+        if not removal["OK"] and "no such file or directory" in str( removal["Message"] ).lower():
+          removalStatus[lfn] = removal["Message"]
+          continue
+        # # not OK but request belongs to DataManager?
+        if not self.requestOwnerDN and \
+           ( not removal["OK"] and "Write access not permitted for this credential." in removal["Message"] ) or \
+           ( removal["OK"] and "Failed" in removal["Value"] and
+             lfn in removal["Value"]["Failed"] and
+             "permission denied" in str( removal["Value"]["Failed"][lfn] ).lower() ):
+          self.debug( "removeFile: retrieving proxy for %s" % lfn )
+          getProxyForLFN = self.getProxyForLFN( lfn )
+          # # can't get correct proxy? continue...
+          if not getProxyForLFN["OK"]:
+            self.warn( "removeFile: unable to get proxy for file %s: %s" % ( lfn, getProxyForLFN["Message"] ) )
+            removal = getProxyForLFN
+          else:
+            # # you're a DataManager, retry with the new one proxy
+            removal = self.replicaManager().removeFile( lfn )
+      finally:
+        # # make sure DataManager proxy is set back in place
+        if not self.requestOwnerDN and self.dataManagerProxy():
+          # # remove temp proxy
+          if os.environ["X509_USER_PROXY"] != self.dataManagerProxy():
+            os.unlink( os.environ["X509_USER_PROXY"] )
+          # # put back DataManager proxy
+          os.environ["X509_USER_PROXY"] = self.dataManagerProxy()
+
+      # # save error
+      if not removal["OK"]:
+        removalStatus[lfn] = removal["Message"]
+        continue
+      # # check fail reason, filter out missing files
+      removal = removal["Value"]
+      if lfn in removal["Failed"]:
+        removalStatus[lfn] = removal["Failed"][lfn]
+
+    # # counters
+    filesRemoved = 0
+    filesFailed = 0
+    subRequestError = []
+    # # update File statuses and errors
+    for lfn, error in removalStatus.items():
+
+      # # set file error if any
+      if error:
+        self.debug( "removeFile: %s: %s" % ( lfn, str( error ) ) )
+        fileError = str( error ).replace( "'", "" )[:255]
+        fileError = requestObj.setSubRequestFileAttributeValue( index, "removal", lfn,
+                                                                "Error", fileError )
+        if not fileError["OK"]:
+          self.error( "removeFile: unable to set Error for %s: %s" % ( lfn, fileError["Message"] ) )
+      # # no error? file not exists? - we are able to recover
+      if not error or "no such file or directory" in str( error ).lower() or \
+            "file does not exist in the catalog" in str( error ).lower():
+        filesRemoved += 1
+        self.info( "removeFile: successfully removed %s" % lfn )
+        updateStatus = requestObj.setSubRequestFileAttributeValue( index, "removal", lfn, "Status", "Done" )
+        if not updateStatus["OK"]:
+          self.error( "removeFile: unable to change status to 'Done' for %s" % lfn )
+      else:
+        filesFailed += 1
+        self.warn( "removeFile: unable to remove file %s : %s" % ( lfn, error ) )
+        errorStr = str( error )
+        if type( error ) == DictType:
+          errorStr = ";".join( [ "%s:%s" % ( key, value ) for key, value in error.items() ] )
+        errorStr = errorStr.replace( "'", "" )
+        subRequestError.append( "%s:%s" % ( lfn, errorStr ) )
+
+    self.addMark( "RemoveFileDone", filesRemoved )
+    self.addMark( "RemoveFileFail", filesFailed )
+
+    # # all 'Done'?
+    if requestObj.isSubRequestDone( index, "removal" )["Value"]:
+      self.info( "removeFile: all files processed, setting subrequest status to 'Done'" )
+      requestObj.setSubRequestStatus( index, "removal", "Done" )
+    elif filesFailed:
+      self.info( "removeFile: all files processed, %s files failed to remove" % filesFailed )
+      subRequestError = ";".join( subRequestError )[:255]
+      subRequestError = requestObj.setSubRequestAttributeValue( index, "removal", "Error", subRequestError )
+
+    return S_OK()
 
