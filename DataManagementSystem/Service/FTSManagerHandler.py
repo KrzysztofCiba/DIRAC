332169896e6eb3e09109795b969d1862f55fb92e
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-10 14:39:13 +0200

    save

--- DataManagementSystem/Service/FTSManagerHandler.py
+++ DataManagementSystem/Service/FTSManagerHandler.py
@@ -93,11 +93,11 @@ class FTSManagerHandler( RequestHandler ):
       # # every hour replace FTSGraph
       gThreadScheduler.addPeriodicTask( FTSHistoryView.INTERVAL , cls.updateFTSStrategy )
 
-      # for i in range( 100 ):
-      #  schedule = cls.ftsStrategy().replicationTree( [ "CERN-USER", "RAL-USER" ], ["PIC-USER"], 1000000 )
-      #  gLogger.always( schedule )
-      # schedule = cls.ftsStrategy().replicationTree( [ "RAL-USER", "CERN-USER"], ["PIC-USER"], 1000000 )
-      # gLogger.always( schedule )
+      for i in range( 100 ):
+        schedule = cls.ftsStrategy().replicationTree( [ "CERN-USER", "RAL-USER" ], ["PIC-USER"], 1000000 )
+        gLogger.always( schedule )
+      schedule = cls.ftsStrategy().replicationTree( [ "RAL-USER", "CERN-USER"], ["PIC-USER"], 1000000 )
+      gLogger.always( schedule )
 
     return S_OK()
 
