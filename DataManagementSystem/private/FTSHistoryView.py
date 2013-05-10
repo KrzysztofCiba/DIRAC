037366789afbdc4299d221220872c554a2e74b14
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-10 12:17:32 +0200

    save

--- DataManagementSystem/private/FTSHistoryView.py
+++ DataManagementSystem/private/FTSHistoryView.py
@@ -67,7 +67,7 @@ class FTSHistoryView( object ):
                           "Size": "SUM(`FTSJob`.`Size`)",
                           "FailedFiles": "SUM(`FTSJob`.`FailedFiles`)",
                           "FailedSize": "SUM(`FTSJob`.`FailedSize`)",
-                          "Completeness": "SUM(`FTSJob`.`Completeness`)/COUNT(DISTINCT `FTSJob`.`FTSJobID`)" },
+                          "Completeness": "SUM(`FTSJob`.`Completeness`)/COUNT(DISTINCT `FTSJob`.`FTSJobID`)/100.0" },
              "SelectFrom" : "`FTSJob`",
              "Clauses": [ "`FTSJob`.`LastUpdate` > ( UTC_TIMESTAMP() - INTERVAL %s SECOND )" % FTSHistoryView.INTERVAL ],
              "GroupBy": [ "`SourceSE`", "`TargetSE`", "`Status`" ] }
