f1fc9eeba59763fb8dcf7e85876eae2cf6dab4cd
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-10 22:03:27 +0200

    save, new fiel FTSGraph

---
+++ DataManagementSystem/private/FTSGraph.py
@@ -0,0 +1,39 @@
+########################################################################
+# $HeadURL $
+# File: FTSGraph.py
+# Author: Krzysztof.Ciba@NOSPAMgmail.com
+# Date: 2013/05/10 20:02:32
+########################################################################
+""" :mod: FTSGraph 
+    =======================
+ 
+    .. module: FTSGraph
+    :synopsis: FTS graph, where nodes are sites and enges are routes
+    .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
+
+    FTS graph, where nodes are sites and enges are routes
+"""
+__RCSID__ = "$Id: $"
+##
+# @file FTSGraph.py
+# @author Krzysztof.Ciba@NOSPAMgmail.com
+# @date 2013/05/10 20:03:00
+# @brief Definition of FTSGraph class.
+
+## imports 
+
+
+########################################################################
+class FTSGraph(object):
+  """
+  .. class:: FTSGraph
+  
+  """
+
+  def __init__( self ):
+    """c'tor
+
+    :param self: self reference
+    """
+    pass
+
