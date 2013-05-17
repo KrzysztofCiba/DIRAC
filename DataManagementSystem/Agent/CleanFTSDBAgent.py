5b5e4337ff5302514ec3117df38a98f9b8cdad88
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-17 09:48:30 +0200

    save, new agent CleanFTSDBAgent

---
+++ DataManagementSystem/Agent/CleanFTSDBAgent.py
@@ -0,0 +1,39 @@
+########################################################################
+# $HeadURL $
+# File: CleanFTSDBAgent.py
+# Author: Krzysztof.Ciba@NOSPAMgmail.com
+# Date: 2013/05/17 07:46:34
+########################################################################
+""" :mod: CleanFTSDBAgent 
+    =======================
+ 
+    .. module: CleanFTSDBAgent
+    :synopsis: cleaning FTSDB from old records
+    .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
+
+    cleaning FTSDB from old records
+"""
+__RCSID__ = "$Id: $"
+##
+# @file CleanFTSDBAgent.py
+# @author Krzysztof.Ciba@NOSPAMgmail.com
+# @date 2013/05/17 07:46:45
+# @brief Definition of CleanFTSDBAgent class.
+
+## imports 
+
+
+########################################################################
+class CleanFTSDBAgent(AgentModule):
+  """
+  .. class:: CleanFTSDBAgent
+  
+  """
+
+  def __init__( self ):
+    """c'tor
+
+    :param self: self reference
+    """
+    pass
+
