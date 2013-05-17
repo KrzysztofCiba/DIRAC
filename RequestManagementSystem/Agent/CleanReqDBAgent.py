e282dbefd002216f6b404db0055eb1cb76c9c1fc
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-17 10:32:36 +0200

    save

---
+++ RequestManagementSystem/Agent/CleanReqDBAgent.py
@@ -0,0 +1,39 @@
+########################################################################
+# $HeadURL $
+# File: CleanReqDBAgent.py
+# Author: Krzysztof.Ciba@NOSPAMgmail.com
+# Date: 2013/05/17 08:31:26
+########################################################################
+""" :mod: CleanReqDBAgent 
+    =======================
+ 
+    .. module: CleanReqDBAgent
+    :synopsis: cleaning RequestDB from obsolete records and kicking assigned requests 
+    .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
+
+    cleaning RequestDB from obsolete records and kicking assigned requests 
+"""
+__RCSID__ = "$Id: $"
+##
+# @file CleanReqDBAgent.py
+# @author Krzysztof.Ciba@NOSPAMgmail.com
+# @date 2013/05/17 08:32:08
+# @brief Definition of CleanReqDBAgent class.
+
+## imports 
+
+
+########################################################################
+class CleanReqDBAgent(AgentModule):
+  """
+  .. class:: CleanReqDBAgent
+  
+  """
+
+  def __init__( self ):
+    """c'tor
+
+    :param self: self reference
+    """
+    pass
+
