953f5d9dbadf2e54e13f45daa2bfb46725b9d59b
Krzysztof Ciba <Krzysztof.Ciba@gmail.com>
2013-05-21 10:14:43 +0200

    save

--- DataManagementSystem/test/ReplicateAndRegisterTests.py
+++ DataManagementSystem/test/ReplicateAndRegisterTests.py
@@ -4,9 +4,9 @@
 # Author: Krzysztof.Ciba@NOSPAMgmail.com
 # Date: 2013/05/13 18:38:55
 ########################################################################
-""" :mod: ReplicateAndRegisterTests 
+""" :mod: ReplicateAndRegisterTests
     ===============================
- 
+
     .. module: ReplicateAndRegisterTests
     :synopsis: unittest for replicateAndRegister operation handler
     .. moduleauthor:: Krzysztof.Ciba@NOSPAMgmail.com
@@ -14,13 +14,13 @@
     unittest for replicateAndRegister operation handler
 """
 __RCSID__ = "$Id: $"
-##
+# #
 # @file ReplicateAndRegisterTests.py
 # @author Krzysztof.Ciba@NOSPAMgmail.com
 # @date 2013/05/13 18:39:13
 # @brief Definition of ReplicateAndRegisterTests class.
 
-# # imports 
+# # imports
 import unittest
 import random
 import os
@@ -32,17 +32,17 @@ from DIRAC.RequestManagementSystem.Client.Operation import Operation
 from DIRAC.RequestManagementSystem.Client.File import File
 
 ########################################################################
-class ReplicateAndRegisterTests(unittest.TestCase):
+class ReplicateAndRegisterTests( unittest.TestCase ):
   """
   .. class:: ReplicateAndRegisterTests
-  
+
   """
   def setUp( self ):
     """ test setup """
     self.fname = "/tmp/testFile"
     self.file = open( self.fname, "w+" )
     for i in range( 100 ):
-      self.file.write( random.randint( 0, i ) )
+      self.file.write( str( random.randint( 0, i ) ) )
     self.file.close()
 
     self.checksum = fileAdler( self.fname )
